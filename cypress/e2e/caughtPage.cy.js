c85083792dbc9b7cd32c0d92f63e547b170db181
Uzaeir <paf_uzair@yahoo.com>
2023-02-03 23:05:51 +0100

    test: rename test file and improve the testcase

---
+++ cypress/e2e/caughtPage.cy.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+describe('Visiting the Caught Page', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.intercept('**/pokemon/151').as('pokemon_api')
+  })
+
+  it('user should see a caught page ', () => {
+    cy.visit('/caught').url().should('include', '/caught')
+  })
+
+  it('User should be able to navigate home using the home button', () => {
+    cy.visit('/')
+      .wait('@pokemon_api')
+      .its('response.statusCode')
+      .should('eq', 200)
+    cy.get('[data-testid="pokemons-lists"]')
+      .find('[data-testid="pokemons"]')
+      .should('have.length', 6)
+      .should('be.visible')
+      .then(mainPage => {
+        cy.get(mainPage)
+          .find('[data-testid="pokemon-1"]')
+          .should('be.visible')
+          .click()
+      })
+  })
+})
