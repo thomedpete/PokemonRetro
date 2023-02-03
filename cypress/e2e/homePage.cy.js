fc29ecc26c7f9552ef3b67110f248d8668071bfd
Uzaeir <paf_uzair@yahoo.com>
2023-02-03 23:07:08 +0100

    test: improve test case

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,38 +1,49 @@
+/* eslint-disable no-undef */
 describe('onLoad', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
-    cy.intercept(`https://pokeapi.co/api/v2/pokemon`,
-      {
-        'id': '1',
-        'name': 'bulbasaur',
-        'status': 200
-      }
-    )
-  });
+    cy.visit('/')
+    cy.intercept(`**/pokemon`, {
+      id: '1',
+      name: 'bulbasaur',
+      status: 200
+    })
+  })
 
   it('Should see the logo of the page', () => {
-    cy.get('.logo')
-  });
+    cy.get('[data-testid="pokemon-logo"]').should('be.visible')
+  })
 
-  it('Should see  random pokemon', () => {
-    cy.get('.card-1').should('be.visible')
-  });
-
+  it('Should see 6 random pokemon', () => {
+    cy.get('[data-testid="pokemons"]')
+      .should('have.length', 6)
+      .should('be.visible')
+  })
 
   it('Should be able to click the Pokedex option and go to Pokedex page', () => {
-    cy.get(`#long-button`).click().get('#Pokedex > .little-link').click();
-  });
+    cy.get('[data-testid="menu-button"]')
+      .click()
+      .get('[data-testid="Pokedex-button"]')
+      .click()
+  })
 
   it('Should be able to click the home option and go to Home page', () => {
-    cy.visit('http://localhost:3000/pokedex');
-    cy.get('.MuiButtonBase-root').click()
-      .url().should('eq', 'http://localhost:3000/')
-  });
+    cy.visit('/pokedex')
+    cy.get('[data-testid="back-to-wild-area-button"]')
+      .click()
+      .url()
+      .should('include', '/')
+  })
 
   it('Should be able to navigate with browser arrows', () => {
-    cy.get(`#long-button`).click().get('#Pokedex > .little-link').click()
-      .url().should('eq', 'http://localhost:3000/pokedex').go('back')
-      .url().should('eq', 'http://localhost:3000/').go('forward')
-  });
-
+    cy.get(`[data-testid="menu-button"]`)
+      .click()
+      .get('[data-testid="Pokedex-button"]')
+      .click()
+      .url()
+      .should('include', '/pokedex')
+      .go('back')
+      .url()
+      .should('include', '/')
+      .go('forward')
+  })
 })
