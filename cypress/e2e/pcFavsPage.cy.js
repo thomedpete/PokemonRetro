fba98bcd502ecf8175810bdf011b0257e1e94f1c
Uzaeir <paf_uzair@yahoo.com>
2023-02-04 23:04:45 +0100

    test: refactor test case

--- cypress/e2e/pcFavsPage.cy.js
+++ cypress/e2e/pcFavsPage.cy.js
@@ -5,21 +5,27 @@ describe('Visiting the PC Page', () => {
   })
 
   it('user should see a PC page ', () => {
-    cy.get('.pcPageHeader').contains('Your PC')
+    cy.contains('[data-testid="pc-logo"]', 'Your PC')
   })
 
   it('user should see a PC page message ', () => {
-    cy.get('.subHeader').contains('The Pokemon you caught live here!')
+    cy.contains(
+      '[data-testid="pc-sub-heading-text"]',
+      'The Pokemon you caught live here!'
+    )
   })
 
   it('user should see a list of pokemon displayed ', () => {
     cy.visit('/')
-    cy.wait(3000).get('.card-1').click()
-    cy.get('.monInfo').should('be.visible')
+    cy.wait(3000).get('[data-testid="pokemon-1"]').click()
+    cy.get('[data-testid="pokemon-info"]').should('be.visible')
   })
 
   it('User should be able to navigate home using the home button', () => {
     cy.visit('/pc').url().should('include', '/pc')
-    cy.get('.MuiButtonBase-root').click().url().should('include', '/')
+    cy.get('[data-testid="home-link-button-from-pc"]')
+      .click()
+      .url()
+      .should('include', '/')
   })
 })
